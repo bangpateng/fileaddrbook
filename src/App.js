346ba4f7b1544469752ccd0daa0061883b31f851
bangpateng <coinnyari@gmail.com>
2025-04-01 15:23:08 +0200

    Remove API integration to improve reliability

--- src/App.js
+++ src/App.js
@@ -3,3 +3,5 @@ export default function App() { return <div>Hello World</div>; }
 // Timestamp: 1743795795
 // App component update
 // Timestamp: 1743795796
+// App component update
+// Timestamp: 1743795797
