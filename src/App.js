bb459230542b296f325d00a650a80f28fd810014
bangpateng <coinnyari@gmail.com>
2025-04-05 14:39:48 +0200

    Remove login functionality for compliance

--- src/App.js
+++ src/App.js
@@ -41,3 +41,5 @@ export default function App() { return <div>Hello World</div>; }
 // Timestamp: 1743795818
 // App component update
 // Timestamp: 1743795819
+// App component update
+// Timestamp: 1743795819
